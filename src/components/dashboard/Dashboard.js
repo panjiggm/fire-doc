328e53f883ee623e33f93ad895537bcff2629cab
Panji Gumelar <panjiggm@gmail.com>
2019-07-26 17:06:50 +0700

    create functions triger for notifications

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -17,10 +17,10 @@ class Dashboard extends Component {
       return ( 
          <div className="dashboard container">
             <div className="row">
-               <div className="col s12 m7">
+               <div className="col s12 m6">
                   <ProjectList projects={projects} />
                </div>
-               <div className="col s12 m3 offset-m1">
+               <div className="col s12 m5 offset-m1">
                   <Notifications />
                </div>
             </div>
